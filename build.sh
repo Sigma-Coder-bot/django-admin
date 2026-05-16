0ed6114f49453163fd2137e54bc04def2806b7e8
Ratnadeep <ratnadeepdas914@email.com>
2026-05-17 03:26:39 +0530

    Fake all Movies migrations to fix conflicts

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --fake Movies 0001_initial
+python manage.py migrate --fake Movies
 python manage.py migrate
 python manage.py createsu
