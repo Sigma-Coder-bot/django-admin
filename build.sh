abafb29941dc0dbdb479b4f2582395c412e15720
Ratnadeep <ratnadeepdas914@email.com>
2026-05-17 03:18:20 +0530

    Fix migration conflict with merge

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations Movies
 python manage.py makemigrations --merge --no-input
-python manage.py migrate --fake-initial
+python manage.py migrate --fake Movies
+python manage.py migrate
 python manage.py createsu
