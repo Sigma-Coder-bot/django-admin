192389008ab3262aef382e4909f8958588581d36
Ratnadeep <ratnadeepdas914@email.com>
2026-05-18 11:56:44 +0530

    Fix migration rollback issue

--- build.sh
+++ build.sh
@@ -3,7 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate Movies zero
-python manage.py migrate Movies
+python manage.py migrate --fake Movies
 python manage.py migrate
 python manage.py createsu
