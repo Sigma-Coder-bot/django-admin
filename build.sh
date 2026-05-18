29f69a3e3f9b200984cf2911b403c37f4b6195a9
Ratnadeep <ratnadeepdas914@email.com>
2026-05-18 12:04:59 +0530

    Fix circular migration dependency

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --fake Movies 0006_genre_language_movie_genres_movie_language_and_more
 python manage.py migrate
 python manage.py createsu
