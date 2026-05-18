5222158597738c7c0f2b8cd8d873de2ecc985ea0
Ratnadeep <ratnadeepdas914@email.com>
2026-05-18 12:57:49 +0530

    Task 5: Genre and language filtering with pagination

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic
 python manage.py makemigrations Movies
 python manage.py makemigrations --merge --no-input
-python manage.py migrate --fake-initial
+python manage.py migrate
 python manage.py migrate
 python manage.py createsu
